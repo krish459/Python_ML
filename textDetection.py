806d5f50b8f2c20de73b2cacc13aa02ea4d02ac1
Krish <krishshah235@gmail.com>
2022-08-18 15:46:29 +0530

    handwritten

--- textDetection.py
+++ textDetection.py
@@ -4,7 +4,7 @@ import pytesseract
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
 
-img = cv2.imread('img5.jpg')
+img = cv2.imread('img6.jpg')
 img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
 print(pytesseract.image_to_string(img))
 
